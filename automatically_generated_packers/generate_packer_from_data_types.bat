088d805969bf39451bd092269bc1313832322761
Yuki Suga <ysuga@ysuga.net>
2020-08-11 16:01:12 +0900

    add generation batch script for Win32

---
+++ automatically_generated_packers/generate_packer_from_data_types.bat
@@ -0,0 +1,4 @@
+
+
+..\build\bin\Debug\nanorosmsgparser.exe ../data_types/std_msgs ../data_types/roscpp  ../data_types/geometry_msgs ../data_types/sensor_msgs ../data_types/nav_msgs ../data_types/actionlib_msgs
+rem
