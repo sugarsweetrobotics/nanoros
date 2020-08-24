8e0ef018008e791480bff5cef8f5e74098222e78
Yuki Suga <ysuga@sugarsweetrobotics.com>
2020-08-24 11:25:05 +0900

    add packer generator shell script

---
+++ automatically_generated_packers/generate_packer_from_data_types.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+../build/bin/nanorosmsgparser ../data_types/std_msgs ../data_types/roscpp  ../data_types/geometry_msgs ../data_types/sensor_msgs ../data_types/nav_msgs ../data_types/actionlib_msgs
+
